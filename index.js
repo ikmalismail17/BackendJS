95b5737ec2d9715390035c9b1d6e938935602047
ikmalismail17 <ikmal5a2013@gmail.com>
2024-01-11 00:15:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import reqRoute from "./routes/uiRequest.js"
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors());
